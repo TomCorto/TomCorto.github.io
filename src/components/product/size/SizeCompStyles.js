232f75f92d0dcb69be96378122bf0ff2b1569118
Tom <bristolthomas@gmail.com>
2018-05-22 01:00:08 +0200

    dimension responsive

--- src/components/product/size/SizeCompStyles.js
+++ src/components/product/size/SizeCompStyles.js
@@ -12,8 +12,8 @@ export const Container = styled.section`
   height: auto;
   width: 100%;
   background-color: #000000;
-  @media all and (max-width: 1024px) { grid-template-columns: 1fr; grid-template-rows: repeat(3, auto); padding: 5vh 0 5vh; grid-row-gap: 1.5em; }
-  @media all and (min-width: 1024px) { grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(2, auto); padding: 5vh 0 5vh; }
+  @media all and (max-width: 1280px) { grid-template-columns: 1fr; grid-template-rows: repeat(3, auto); padding: 5vh 0 5vh; grid-row-gap: 1.5em; }
+  @media all and (min-width: 1280px) { grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(2, auto); padding: 5vh 0 5vh; }
   @media all and (min-width: 1440px) { padding: 6vh 0 6vh; }
 `
 
@@ -21,14 +21,14 @@ export const ImgFrame = styled.img`
   /*border: 1px solid white;*/
 `
 export const ImgGrid = styled.div`
-  @media all and (max-width: 1024px) { grid-area: 1 / 1 / 2 / -1; }
-  @media all and (min-width: 1024px) { grid-area: 1 / 1 / 1 / 2; }
+  @media all and (max-width: 1280px) { grid-area: 1 / 1 / 2 / -1; }
+  @media all and (min-width: 1280px) { grid-area: 1 / 1 / 1 / 2; }
   /*border:1px solid yellow;*/
 `
 
 export const TableGrid = styled.div`
-  @media all and (max-width: 1024px) { grid-area: 3 / 1 / 4 / -1; justify-self: center; }
-  @media all and (min-width: 1024px) { grid-area: 1 / 2 / 1 / -1; }
+  @media all and (max-width: 1280px) { grid-area: 3 / 1 / 4 / -1; justify-self: center; }
+  @media all and (min-width: 1280px) { grid-area: 1 / 2 / 1 / -1; }
   justify-self: start;
   /*border: 1px solid blue;*/
 `
@@ -36,8 +36,8 @@ export const TableGrid = styled.div`
 export const ButtonGrid = styled.div`
   overflow: hidden;
   justify-self: center;
-  @media all and (max-width: 1024px) { grid-area: 4 / 1 / -1 / -1; }
-  @media all and (min-width: 1024px) { grid-area: 2 / 1 / 2 / -1; }
+  @media all and (max-width: 1280px) { grid-area: 4 / 1 / -1 / -1; }
+  @media all and (min-width: 1280px) { grid-area: 2 / 1 / 2 / -1; }
 `
 
 
